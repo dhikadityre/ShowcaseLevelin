9d40869d3d65cbfdc709fc867074834f8274f78b
@algoreadme <dhika.aditya@kpvendor.id>
2025-06-30 22:14:16 +0700

    Initial Commit

---
+++ ShowcaseLevelin/ShowcaseLevelinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowcaseLevelinApp.swift
+//  ShowcaseLevelin
+//
+//  Created by @algoreadme on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ShowcaseLevelinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
